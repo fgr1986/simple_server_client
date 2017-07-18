59fdf666b560bc67691620572d89044d0ed8e61f
Daniel Perez de Andres <danielperezdeandres@gmail.com>
2017-07-18 15:16:41 +0200

    Client: Varias hebras ejecutan simultaneamente varios clientes
    Ahora mismo es muy simple la implementación ya que no hay memoria
    compartida entre hebras, y por tanto no hay riesgo de condiciones de
    carrera.
    
    Signed-off-by: Daniel Perez de Andres <danielperezdeandres@gmail.com>

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -9,16 +9,38 @@
 
 #include "client.hpp"
 
+void client_thread(void)
+{
+	Client client;
 
+	if(client.connect()) {
+		client.send();
+	}
+	else {
+		std::cout << "Client in thread [" << std::this_thread::get_id() << "] could not connect to the server" << std::endl;
+	}
+}
 
 int main (int argc, char* argv[])
 {
-	try {
-		Client client;
+	int n_clients = 5;
+	std::vector<std::thread> th_pool;
 
-		if (client.connect()) {
-			client.send();
+	if (argc > 1) {
+		std::string arg_client(argv[1]);
+		n_clients = stoi(arg_client);
+	}
+
+	try {
+		for (int i = 0; i < n_clients; ++i) {
+			th_pool.push_back(std::thread(client_thread));
 		}
+
+		for(auto& t: th_pool) {
+			t.join();
+		}
+
+
 
 	}
 
